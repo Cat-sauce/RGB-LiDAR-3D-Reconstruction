da06e286b38eda03c16028490ecd92d9d8cd5934
Unpopular opinion <141830345+Cat-sauce@users.noreply.github.com>
2026-08-22 17:49:36 +0530

    Create test_environment.py

---
+++ preprocessing/test_environment.py
@@ -0,0 +1,13 @@
+import cv2
+import numpy as np
+import open3d as o3d
+import matplotlib
+import pandas as pd
+from PIL import Image
+
+print("Environment check successful!")
+
+print("OpenCV:", cv2.__version__)
+print("NumPy:", np.__version__)
+print("Pandas:", pd.__version__)
+print("Open3D:", o3d.__version__)
